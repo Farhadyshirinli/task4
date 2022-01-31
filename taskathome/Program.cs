4ecfd106a96f912a900ad090fb3cfae229331341
farhadyshirinli <farhadysh@code.edu.az>
2022-02-01 00:04:29 +0400

    tuboazcar

---
+++ taskathome/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace taskathome
+{
+    internal class Program
+    {
+     
+        static void Main()
+        {
+            car auto = new car();
+            {
+
+                auto.Make = "Toyota";
+                auto.Model = "Corolla";
+                auto.year = 2013;
+                auto.engine = 1.6;
+                auto.color = "black";
+                auto.horsepower = 128;
+                auto.fuel = "gasoline";
+                auto.transmisson = "automatic";
+                auto.mileage = 160.000;
+                auto.condition = "used";
+
+
+                System.Console.WriteLine(auto);
+
+            }
+
+
+
+
+        }
+
+
+
+
+
+      }
+}
---
+++ taskathome/car.cs
@@ -0,0 +1,29 @@
+﻿namespace taskathome
+{
+    internal class car
+
+    {        
+        
+        public string Make;
+        public string Model;
+        public int year;
+        public double engine;
+        public int horsepower;
+        public string color;
+        public string  fuel;
+        public string transmisson;
+        public double  mileage;
+        public string condition;
+
+
+
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} {year} {engine} {color} {horsepower} {fuel} { transmisson} { mileage} {condition}";
+        }
+        
+
+    }
+     
+}
